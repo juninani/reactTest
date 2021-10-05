027e37c78c264012ed38d38ca9f7246366849b17
LEEJUNHEE <juninani123@gmail.com>
2021-10-05 15:13:13 +0900

    출퇴 색상

--- src/Say.js
+++ src/Say.js
@@ -1,18 +1,20 @@
-import React,{ useState } from "react";
-
+import React, { useState } from "react";
 
 const Say = () => {
-    const [message,setMessege] = useState('');
-    const onClickEnter =() => setMessege('입장');
-    const onClickLeave = () => setMessege('퇴장');
-    
-    return (
-        <div>
-          <button onClick={onClickEnter}>입장</button>  
-          <button onClick={onClickLeave}>퇴장</button>
-          <h1>{message}</h1>
-        </div>
-    );
+  const [message, setMessege] = useState("");
+  const onClickEnter = () => setMessege("입장");
+  const onClickLeave = () => setMessege("퇴장");
+  const [color, setColor] = useState("black");
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{color:'red' }} onClick={() => setColor("red")}>빨강</button>
+      <button style={{color:'green'}} onClick={() => setColor("green")}>초록</button>
+      <button style={{color:'blue'}}onClick={() => setColor("blue")}>파랑</button>
+    </div>
+  );
 };
 
 export default Say;
